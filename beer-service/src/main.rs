6e344847adce6c05ed7c1ac36db7f322dec25184
Christoph Welcz <christoph@welcz.de>
2023-04-23 19:35:32 +0200

    🔧 using default address instead of localhost to work in a container as well

--- beer-service/src/main.rs
+++ beer-service/src/main.rs
@@ -14,7 +14,7 @@ mod repository;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   env_logger::init_from_env(Env::default().default_filter_or("info"));
-  let address = ("localhost", 8080);
+  let address = ("0.0.0.0", 8080);
   info!("starting server on {address:?}");
   let db = BeerRepository::init().await;
   let db_data = Data::new(db);
--- beer-service/src/repository.rs
+++ beer-service/src/repository.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use dotenv::dotenv;
 use futures::TryStreamExt;
+use log::info;
 use mongodb::bson::doc;
 use mongodb::bson::oid::ObjectId;
 use mongodb::error::Result;
@@ -10,62 +11,68 @@ use mongodb::{Client, Collection};
 use crate::model::Beer;
 
 pub struct BeerRepository {
-    col: Collection<Beer>,
+  col: Collection<Beer>,
 }
 
 impl BeerRepository {
-    pub(crate) async fn init() -> Self {
-        dotenv().ok();
-        let uri =
-            env::var("MONGODB_URI").expect("you need to set a proper MONGODB_URI in your .env");
-        let client = Client::with_uri_str(uri)
-            .await
-            .expect("could not instantiate MongoDB client");
-        let db = client.database("beersdb");
-        let col = db.collection("Beer");
-        BeerRepository { col }
-    }
+  pub(crate) async fn init() -> Self {
+    dotenv().ok();
+    let uri =
+      env::var("MONGODB_URI").expect("you need to set a proper MONGODB_URI in your environment");
+    let client = Client::with_uri_str(uri)
+      .await
+      .expect("could not instantiate MongoDB client");
+    let dbs = client
+      .list_database_names(None, None)
+      .await
+      .expect("could not obtain any databases")
+      .join(",");
+    info!("available databases {}", dbs);
+    let db = client.database("beersdb");
+    let col = db.collection("Beer");
+    BeerRepository { col }
+  }
 
-    pub(crate) async fn find_all_beers(&self) -> Result<Vec<Beer>> {
-        let col = self.col.clone();
-        let cursor = col.find(None, None).await?;
-        let beers: Vec<Beer> = cursor.try_collect().await?;
-        Ok(beers)
-    }
+  pub(crate) async fn find_all_beers(&self) -> Result<Vec<Beer>> {
+    let col = self.col.clone();
+    let cursor = col.find(None, None).await?;
+    let beers: Vec<Beer> = cursor.try_collect().await?;
+    Ok(beers)
+  }
 
-    pub(crate) async fn find_beer(&self, id: ObjectId) -> Result<Option<Beer>> {
-        let col = self.col.clone();
-        let filter = doc! {"_id": id};
-        let maybe_beer = col.find_one(filter, None).await?;
-        Ok(maybe_beer)
-    }
+  pub(crate) async fn find_beer(&self, id: ObjectId) -> Result<Option<Beer>> {
+    let col = self.col.clone();
+    let filter = doc! {"_id": id};
+    let maybe_beer = col.find_one(filter, None).await?;
+    Ok(maybe_beer)
+  }
 
-    pub(crate) async fn create_beer(&self, beer: Beer) -> Result<Beer> {
-        let col = self.col.clone();
-        let created = col.insert_one(beer.clone(), None).await?;
-        let new_beer = Beer {
-            id: created.inserted_id.as_object_id(),
-            ..beer.clone()
-        };
-        Ok(new_beer)
-    }
+  pub(crate) async fn create_beer(&self, beer: Beer) -> Result<Beer> {
+    let col = self.col.clone();
+    let created = col.insert_one(beer.clone(), None).await?;
+    let new_beer = Beer {
+      id: created.inserted_id.as_object_id(),
+      ..beer.clone()
+    };
+    Ok(new_beer)
+  }
 
-    pub(crate) async fn delete_beer(&self, id: ObjectId) -> Result<()> {
-        let col = self.col.clone();
-        let filter = doc! {"_id": id};
-        col.delete_one(filter, None).await?;
-        Ok(())
-    }
+  pub(crate) async fn delete_beer(&self, id: ObjectId) -> Result<()> {
+    let col = self.col.clone();
+    let filter = doc! {"_id": id};
+    col.delete_one(filter, None).await?;
+    Ok(())
+  }
 
-    pub(crate) async fn update_beer(&self, beer: Beer) -> Result<Option<Beer>> {
-        let col = self.col.clone();
-        let updated = beer.clone();
-        let filter = doc! {"_id": beer.id};
-        let update = doc! {"$set": {"name": beer.name, "strength": beer.strength}};
-        let result = col.update_one(filter, update, None).await?;
-        Ok(match result.matched_count {
-            0 => None,
-            _ => Some(updated),
-        })
-    }
+  pub(crate) async fn update_beer(&self, beer: Beer) -> Result<Option<Beer>> {
+    let col = self.col.clone();
+    let updated = beer.clone();
+    let filter = doc! {"_id": beer.id};
+    let update = doc! {"$set": {"name": beer.name, "strength": beer.strength}};
+    let result = col.update_one(filter, update, None).await?;
+    Ok(match result.matched_count {
+      0 => None,
+      _ => Some(updated),
+    })
+  }
 }
